3deba5c21716da0959d0a451bf6b0c9425fc7dc9
BD Himes <b@latent.to>
2026-03-30 15:46:24 +0200

    Add stubs for better typing

--- scalecodec/base.pyi
+++ scalecodec/base.pyi
@@ -4,7 +4,6 @@ from typing import Literal
 
 from scalecodec._scale_bytes import ScaleBytes
 
-
 class ScaleDecoder(ABC):
     runtime_config: RuntimeConfigurationObject
     data: Optional[ScaleBytes]
@@ -15,9 +14,7 @@ class ScaleDecoder(ABC):
     def decode(self, check_remaining: bool = True) -> Any: ...
     def encode(self, value: Any) -> ScaleBytes: ...
 
-
 class ScaleType(ScaleDecoder, ABC): ...
-
 
 class RuntimeConfigurationObject:
     config_id: Optional[str]
@@ -32,16 +29,16 @@ class RuntimeConfigurationObject:
         only_primitives_on_init: bool = False,
         implements_scale_info: bool = False,
     ) -> None: ...
-
-    def get_decoder_class(self, type_string: Union[str, dict]) -> Optional[type[ScaleType]]: ...
-
-    def batch_decode(self, type_strings: list[str], data_list: list[bytes]) -> list[Any]: ...
-
+    def get_decoder_class(
+        self, type_string: Union[str, dict]
+    ) -> Optional[type[ScaleType]]: ...
+    def batch_decode(
+        self, type_strings: list[str], data_list: list[bytes]
+    ) -> list[Any]: ...
     def update_type_registry(self, type_registry: dict) -> None: ...
     def update_type_registry_types(self, types_dict: dict) -> None: ...
     def clear_type_registry(self) -> None: ...
     def add_portable_registry(self, metadata: Any) -> None: ...
-
     @overload
     def create_scale_object(
         self,
@@ -49,7 +46,6 @@ class RuntimeConfigurationObject:
         data: Optional[ScaleBytes] = None,
         **kwargs: Any,
     ) -> "GenericMetadataVersioned": ...
-
     @overload
     def create_scale_object(
         self,
@@ -57,7 +53,6 @@ class RuntimeConfigurationObject:
         data: Optional[ScaleBytes] = None,
         **kwargs: Any,
     ) -> "GenericMetadataAll": ...
-
     @overload
     def create_scale_object(
         self,
@@ -66,9 +61,7 @@ class RuntimeConfigurationObject:
         **kwargs: Any,
     ) -> ScaleType: ...
 
-
 class RuntimeConfiguration(RuntimeConfigurationObject): ...
-
 
 # Imported here so callers only need `from scalecodec.base import ...`
 from scalecodec.types import GenericMetadataVersioned as GenericMetadataVersioned
